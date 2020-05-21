64affebbdb0f8dc1f701a26485422ea5535b24c0
CWMS3 <CWMS3@c20502123.home>
2020-05-21 21:41:20 +0100

    adding package for data parsing

---
+++ src/ssnet/Ssnet.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ssnet;
+
+/**
+ *
+ * @author CWMS3
+ */
+public class Ssnet {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
