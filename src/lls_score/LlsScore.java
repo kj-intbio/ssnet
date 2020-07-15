e83aa27dbe945ca1085fe909385445cf057d6ad2
CWMS3 <CWMS3@c20502123.home>
2020-07-15 15:56:40 +0100

    adding package for log likelihood score

---
+++ src/lls_score/LlsScore.java
@@ -0,0 +1,227 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lls_score;
+
+import bioGrid.*;
+import gold_standard.GoldStandard;
+import relnet.DataSetScorer;
+
+import java.io.*;
+import java.util.*;
+
+/*--------------------------------------------------------------------*\
+|                        Class LlsScore.java                              |
+|               Class which takes the gold standard                    |
+|      scores each datasets confidence using the Lee LLS method        |
+|																	   |
+|                    Author: Katherine James						   |
+|                      Commenced: 28/01/08                             |
+|                    Last Edited: 28/04/09                             |
+\*--------------------------------------------------------------------*/
+public class LlsScore implements DataSetScorer {
+
+    private GoldStandard kegg;
+
+    public LlsScore() {
+    }
+
+    public LlsScore(GoldStandard kegg) {
+        this.kegg = kegg;
+    }
+
+    public GoldStandard getKegg() {
+        return kegg;
+    }
+
+    public void setKegg(GoldStandard kegg) {
+        this.kegg = kegg;
+    }
+
+    public Map<String, Double> scoredDataSet(BioGRID bio) {
+        System.out.println("getting lls...");
+        Map<String, Double> dataSetToScore = new HashMap<String, Double>();
+        double highScore = 0.0;//to find the highest score
+
+        for (String dataSet : bio.getBioGRID().keySet()) {//iterate through the datasets
+
+            System.out.println("calculating for " + dataSet + "...");
+            Double lls = new NonParaLLS().logScore(kegg, bio.getBioGRID().get(dataSet));//score
+
+            if (lls > 0 && !lls.isNaN())
+            {
+                System.out.println(lls + " is the LLS for " + dataSet);
+                //write to file
+                dataSetToScore.put(dataSet, lls);
+
+                if (lls > highScore && !lls.isInfinite()) {
+                    highScore = lls;//to keep track of the highest score
+                }
+            } else {
+                System.out.println(lls + " is the LLS for " + dataSet);
+                System.out.println(dataSet + " will not be used for integration :(");
+                //we don't use the negative scores
+            }
+        }
+
+        double finalHighScore = Math.ceil(highScore + 1);//round up the highscore
+        System.out.println("scoring done");
+        System.out.println("highest score is " + highScore);
+        System.out.println("high score to be used " + finalHighScore);
+
+        //replace Infinity scores with finalHighScore
+        for(String dataSet: new HashSet<String>(dataSetToScore.keySet()))
+        {
+            if (dataSetToScore.get(dataSet).isInfinite())
+            {
+                 dataSetToScore.put(dataSet, finalHighScore);
+            }
+        }
+
+        String outFile = "V" + bio.getVersion() + "LLS.txt";
+        System.out.println("writing to " + outFile);
+
+        //write to file
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+
+            for (String s : dataSetToScore.keySet()) {
+                out.write(s + "\t" + dataSetToScore.get(s) + "\n");
+            }
+            out.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Somethings gone horribly wrong, check your file path");
+        }
+        return dataSetToScore;//the confidence scores
+    //END OF GETSCORES
+    }
+    
+    
+    
+    public Map<String, Double> scoredGoldStandard(List<BioGenePair> gs, int version, Map<String, Double> htp_lls) {
+    
+        Map<String, Double> gsToScore = new HashMap<String, Double>();
+        double highScore = 0.0;
+        
+        Set<String> dataTypes = new HashSet<String>();
+        
+        for(BioGenePair p: gs){
+            dataTypes.add(p.getType());
+        }
+        
+        Map<String, List<BioGenePair>> genePairByType = new HashMap<String, List<BioGenePair>>();
+        for (String type : dataTypes) {
+            genePairByType.put(type, new ArrayList<BioGenePair>());
+        }
+        for (BioGenePair bgp : gs) {
+            genePairByType.get(bgp.getType()).add(bgp);
+        }
+
+        Map<String, Set<Pair>> datasets = new HashMap<String, Set<Pair>>();
+        for (String type : genePairByType.keySet()) {
+            List<BioGenePair> pairList = genePairByType.get(type);
+            if (!pairList.isEmpty()) {
+                Set<Pair> pairs = new HashSet<Pair>();
+                for (BioGenePair pgp : pairList) {
+                    pairs.add(new Pair(pgp.getOrf1(), pgp.getOrf2()));
+                }
+                datasets.put(type, pairs);
+            }
+        }
+
+        System.out.println("Number of gold standard datasets: " + datasets.size());
+        
+        //now we need to score them one by one
+        
+        for(String s: datasets.keySet()){
+                    
+            System.out.println("Scoring dataset: " + s);
+            Set<Pair> gs_dataset = new HashSet<Pair>();
+            
+            gs_dataset = datasets.get(s);
+
+            Set<Pair> gs_goldstandard  = new HashSet<Pair>();
+            
+            for(String t: datasets.keySet()){
+                if(!s.equals(t)){
+                   gs_goldstandard.addAll(datasets.get(t));
+                }
+                
+            }
+            
+            Set<String> allGenes = new HashSet<String>();//all the possible genes
+            Set<Pair> posPairs = new HashSet<Pair>();
+        
+        for (Pair bgp : gs_goldstandard) {
+            allGenes.add(bgp.getOrf1());
+            allGenes.add(bgp.getOrf2());
+            
+            posPairs.add(new Pair(bgp.getOrf1(), bgp.getOrf2()));
+        }
+
+        int numGenes = allGenes.size();
+        System.out.println("number of gs genes " + numGenes);
+        System.out.println("actual number of gs pairs is " + posPairs.size());
+
+        GoldStandard temp_GS = new GoldStandard(posPairs, allGenes);
+          
+        Double lls = new NonParaLLS().logScore(temp_GS, gs_dataset);//score
+
+            if (lls > 0 && !lls.isNaN())
+            {
+                System.out.println(lls + " is the LLS for " + s);
+                //write to file
+                gsToScore.put(s, lls);
+
+                if (lls > highScore && !lls.isInfinite()) {
+                    highScore = lls;//to keep track of the highest score
+                }
+            } else {
+                System.out.println(lls + " is the LLS for " + s);
+                System.out.println(s + " will not be used for integration :(");
+                //we don't use the negative scores
+            }
+        }
+
+        double finalHighScore = Math.ceil(highScore + 1);//round up the highscore
+        System.out.println("scoring done");
+        System.out.println("highest score is " + highScore);
+        System.out.println("high score to be used " + finalHighScore);
+
+        //replace Infinity scores with finalHighScore
+        for(String dataSet: new HashSet<String>(gsToScore.keySet()))
+        {
+            if (gsToScore.get(dataSet).isInfinite())
+            {
+                 gsToScore.put(dataSet, finalHighScore);
+            }
+        }
+
+        String outFile = "V" + version + "GSLLS.txt";
+        System.out.println("writing to " + outFile);
+
+        //write to file
+        try {
+            BufferedWriter out = new BufferedWriter(new FileWriter(outFile));
+
+            for (String s : gsToScore.keySet()) {
+                out.write(s + "\t" + gsToScore.get(s) + "\n");
+            }
+            out.close();
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
+            System.out.println("Somethings gone horribly wrong, check your file path");
+        }
+          
+        gsToScore.putAll(htp_lls);
+       
+        return gsToScore;
+    }
+}
+
+
---
+++ src/lls_score/NonParaLLS.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lls_score;
+
+import bioGrid.*;
+import gold_standard.GoldStandard;
+import java.util.Set;
+/*----------------------------------------------------------------*\
+|                         Class NonParaLLS.java                    |
+|        	    Calculate the LSS for a dataset                |
+|                                                                  |
+|                        Author: Olly Shaw                         |
+|                        Author: Jen Hallinan                      |
+|                      Author: Katherine James                     |
+|                       Commenced: 07/08/07                        |
+|                      Last edited: 15/07/20                       |
+\*----------------------------------------------------------------*/
+public class NonParaLLS {
+
+    //**********Method to score the dataset****************
+    //this is olly lls code adapted for the pipeline
+    public double logScore(GoldStandard gs, Set<Pair> data) {
+
+
+        //get the goldstandards
+        Set<Pair> gsPairs = gs.getPositive();
+
+        int truePosData = pairsInGoldStandard(gsPairs, data);//true positives
+        int totalGenesGs = gs.getGenes().size();//size of kegg
+        int truePosGs = gsPairs.size();//number pos pairs
+        int possiblePairsGs = (totalGenesGs * (totalGenesGs - 1)) / 2;
+        int trueNegGs = gs.numberOfNegatives();//number negative pairs
+        int falsePosData = pairsNotInGoldStandard(kegg, data);//false positives
+
+        //calculate lls
+        double PLE = 1.0 * truePosData / data.size();
+        double notPLE = 1.0 * falsePosData / data.size();
+        double PL = 1.0 * truePosGs / possiblePairsGs;
+        double notPL = 1.0 * trueNegGs / possiblePairsGs;
+
+        double PLE_over_notPLE = PLE / notPLE;
+
+        double PL_over_notPL = PL / notPL;//priors
+
+        double LS = PLE_over_notPLE / PL_over_notPL;
+        double LLS = Math.log(LS);
+
+        return LLS;//the score for the dataset
+    }
+    //END OF LOGSCORE METHOD
+
+    //**********Method to count the number of pairs present in the GS********
+    private static int pairsInGoldStandard(Set<Pair> goldList, Set<Pair> data) {
+
+        int counter = 0;
+        for (Pair pair : data)//the test list
+        {
+            if (goldList.contains(pair))//compare to the gold standard
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static int pairsNotInGoldStandard(GoldStandard gs, Set<Pair> data)
+    {
+        int counter = 0;
+        for(Pair pair : data)
+        {
+            if(!gs.getPositive().contains(pair)
+                && gs.getGenes().contains(pair.getOrf1())
+                && gs.getGenes().contains(pair.getOrf2()))
+            {
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
